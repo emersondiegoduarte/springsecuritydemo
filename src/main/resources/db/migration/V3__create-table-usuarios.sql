e112c2933d45242fbc50aa001f6629ebd4c60203
diegodias <emerson.diego.duarte@gmail.com>
2026-02-17 10:18:39 -0300

    ajustes entidades de usuarios e rolee

--- src/main/resources/db/migration/V3__create-table-usuarios.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usuarios(
-    id BIGSERIAL NOT NULL,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(100) NOT NULL,
-
-    PRIMARY KEY(id)
-);
--- src/main/resources/db/migration/V4__create-table-pacientes.sql
+++
@@ -1,9 +0,0 @@
-create table pacientes(
-    id bigserial not null,
-    nome varchar(100) not null,
-    email varchar(100) not null unique,
-    telefone varchar(100) not null unique,
-    cpf varchar(14) not null unique,
-
-    primary key(id)
-);
